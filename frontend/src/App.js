5da0c5b9d26a276cf18ab7e5fd69a8b553726d5f
Aminul Islam Alvi <aminulialvi@gmail.com>
2021-05-02 18:08:15 +0600

    Added sample header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Header, Footer } from "./components";
 
 function App() {
   return (
-    <div className="App">
-      Hello World this is awesome
+    <div>
+      <Header />
+      <Footer />
     </div>
   );
 }
---
+++ frontend/src/components/index.js
@@ -0,0 +1,2 @@
+export { Header } from "./Header/header";
+export { Footer } from "./Footer/footer";
