52356ecda2b2436fd06f62ecf38c46a37636afb8
ArmanKamal <armankamal2421@gmail.com>
2021-05-02 05:45:10 -0500

    Project Setup

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hello World this is awesome
+    </div>
+  );
+}
+
+export default App;
